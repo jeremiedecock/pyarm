fbf2354dee74d7b4c8cf5bee82c0f84e8ac73a19
jeremie.decock.dmz <jeremie.decock.dmz@bb945c2a-ced2-7b97-2cc3-da1a3ce42252>
2010-06-04 16:27:37 +0000

    Add some graphical elements

--- tkinter_gui.py
+++ tkinter_gui.py
@@ -3,6 +3,7 @@
 # Copyright (c) 2010 Jérémie DECOCK (http://www.jdhp.org)
 
 import Tkinter as tk
+import numpy as np
 import math
 import time
 import fig
@@ -23,10 +24,12 @@ class GUI:
     str_var1 = None
     str_var2 = None
 
-    points = None
+    initial_angle = 0
+    #initial_angle = math.pi / 2
+
     canevas_width = 800
     canevas_height = 600
-    LENGTH_SCALE = 300. # px/m (pixels per meter)
+    LENGTH_SCALE = 450. # px/m (pixels per meter)
 
     keyboard_flags = [0, 0, 0, 0, 0, 0]
 
@@ -103,25 +106,77 @@ class GUI:
         elif event.char == '6':
             self.keyboard_flags[5] = 0
 
-    def update_shapes_position(self, shoulder_angle, elbow_angle):
-        p0 = (self.canevas_width / 2., self.canevas_height / 2.)
-        p1 = (p0[0] + math.cos(shoulder_angle) * self.arm.upperarm_length * self.LENGTH_SCALE,
-              p0[1] + math.sin(shoulder_angle) * self.arm.upperarm_length * self.LENGTH_SCALE)
-        p2 = (p1[0] + math.cos(shoulder_angle + elbow_angle) * self.arm.forearm_length * self.LENGTH_SCALE,
-              p1[1] + math.sin(shoulder_angle + elbow_angle) * self.arm.forearm_length * self.LENGTH_SCALE)
-        self.points  = (p0, p1, p2)
-
-    def draw_shapes(self):
-        # TODO : optimiser l'affichage (le nb de FPS chute très vite), surveiller consomation memoire
+    def draw_shapes(self, shoulder_angle, elbow_angle):
+        # TODO : only delete moving parts (not the background rectangle of example)
         for tag in self.canvas.find_all():
             self.canvas.delete(tag)
 
         self.canvas.create_rectangle((1,1,800,600), fill="white", outline="black")
 
-        self.canvas.create_line(self.points[0] + self.points[1], fill="black", width=5)
-        self.canvas.create_line(self.points[1] + self.points[2], fill="black", width=5)
+        # Draw the initial angle arrow
+        arrow_point1 = np.array([10, 10]) + self.LENGTH_SCALE / 10
+        arrow_point2 = np.array([math.cos(self.initial_angle),  \
+                                 math.sin(self.initial_angle)]) \
+                       * self.LENGTH_SCALE / 10 + arrow_point1
+        arrow_point3 = np.array([math.cos(self.initial_angle + math.pi/2),  \
+                                 math.sin(self.initial_angle + math.pi/2)]) \
+                       * self.LENGTH_SCALE / 20 + arrow_point1
+        self.canvas.create_line(arrow_point1.tolist() + arrow_point2.tolist())
+        self.canvas.create_line(arrow_point1.tolist() + arrow_point3.tolist())
 
-        #self.canvas.create_oval(self.points[0] + (20, 20), outline="black")
+        #####
+        p0 = np.array([self.canevas_width, self.canevas_height]) / 2.
+        p1 = np.array([math.cos(self.initial_angle + shoulder_angle),  \
+                       math.sin(self.initial_angle + shoulder_angle)]) \
+             * self.arm.upperarm_length * self.LENGTH_SCALE + p0
+        p2 = np.array([math.cos(self.initial_angle + shoulder_angle + elbow_angle),  \
+                       math.sin(self.initial_angle + shoulder_angle + elbow_angle)]) \
+             * self.arm.upperarm_length * self.LENGTH_SCALE + p1
+
+        # Draw angles
+        shoulder_oval_point1 = p0 - 25
+        shoulder_oval_point2 = p0 + 25
+        self.canvas.create_arc(shoulder_oval_point1.tolist() + shoulder_oval_point2.tolist(),
+                               start=0, extent=270, fill="red")
+
+        elbow_oval_point1 = p1 - 25
+        elbow_oval_point2 = p1 + 25
+        self.canvas.create_arc(elbow_oval_point1.tolist() + elbow_oval_point2.tolist(),
+                               start=0, extent=270, fill="red")
+
+        angle_point1 = p0
+        angle_point2 = np.array([math.cos(self.initial_angle),  \
+                                 math.sin(self.initial_angle)]) \
+                       * 30 + p0
+        angle_point3 = p1
+        angle_point4 = np.array([math.cos(self.initial_angle + shoulder_angle),  \
+                                 math.sin(self.initial_angle + shoulder_angle)]) \
+                       * 30 + p1
+
+        self.canvas.create_line(angle_point1.tolist() + angle_point2.tolist(), width=1)
+        self.canvas.create_line(angle_point3.tolist() + angle_point4.tolist(), width=1)
+
+        # Draw angles bounds
+
+        # Draw limbs
+        self.canvas.create_line(p0.tolist() + p1.tolist(), fill="black", width=5)
+        self.canvas.create_line(p1.tolist() + p2.tolist(), fill="black", width=5)
+
+        # Draw joints
+        if hasattr(self.muscle, 'A'):
+            shoulder_point1 = p0 - self.muscle.A[0,0] * self.LENGTH_SCALE
+            shoulder_point2 = p0 + self.muscle.A[0,0] * self.LENGTH_SCALE
+            shoulder_point3 = p0 - self.muscle.A[4,0] * self.LENGTH_SCALE
+            shoulder_point4 = p0 + self.muscle.A[4,0] * self.LENGTH_SCALE
+            self.canvas.create_oval(shoulder_point1.tolist() + shoulder_point2.tolist())
+            self.canvas.create_oval(shoulder_point3.tolist() + shoulder_point4.tolist())
+
+            elbow_point1 = p1 - self.muscle.A[2,1] * self.LENGTH_SCALE
+            elbow_point2 = p1 + self.muscle.A[2,1] * self.LENGTH_SCALE
+            elbow_point3 = p1 - self.muscle.A[4,1] * self.LENGTH_SCALE
+            elbow_point4 = p1 + self.muscle.A[4,1] * self.LENGTH_SCALE
+            self.canvas.create_oval(elbow_point1.tolist() + elbow_point2.tolist())
+            self.canvas.create_oval(elbow_point3.tolist() + elbow_point4.tolist())
 
     def run(self):
         # The main loop
@@ -146,8 +201,7 @@ class GUI:
                 # Update the caneva
                 self.str_var1.set("Shoulder : angle = %1.2frd (%03d°)     velocity = %1.2frd/s     acceleration = %1.2frd/s/s     torque = %03dN.m" % (theta[0], math.degrees(theta[0]), omega[0], alpha[0], tau[0]))
                 self.str_var2.set("Elbow : angle = %1.2frd (%03d°)     velocity = %1.2frd/s     acceleration = %1.2frd/s/s     torque = %03dN.m" % (theta[1], math.degrees(theta[1]), omega[1], alpha[1], tau[1]))
-                self.update_shapes_position(*theta)
-                self.draw_shapes()
+                self.draw_shapes(*theta)
 
                 self.root.update_idletasks() # redraw
                 self.root.update() # process events
