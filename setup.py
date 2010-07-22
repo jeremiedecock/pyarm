131bcf2974a0aa5d34327213cad4cbb2b3623300
jeremie.decock.dmz <jeremie.decock.dmz@bb945c2a-ced2-7b97-2cc3-da1a3ce42252>
2010-07-22 11:30:32 +0000

    Add distutils.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2010 Jérémie DECOCK (http://www.jdhp.org)
+
+from distutils.core import setup
+
+setup(author='Jérémie DECOCK',
+      author_email='gremy@tuxfamily.org',
+      description='A robotic arm model and simulator',
+      license='MIT license',
+      name='pyarm',
+      packages=['pyarm'],
+      platforms=['unix'],
+      url='http://code.google.com/p/pyarm/',
+      version='0.1')
+
