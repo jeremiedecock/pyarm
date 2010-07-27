68babe2de9a8204c46ad23e1c82dd0ff8fe44c94
jeremie.decock.dmz <jeremie.decock.dmz@bb945c2a-ced2-7b97-2cc3-da1a3ce42252>
2010-07-27 13:57:27 +0000

    Add a unittest on plot_figs module.

---
+++ pyarm/tests/test_plot_figs.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2010 Jérémie DECOCK (http://www.jdhp.org)
+
+import unittest
+import os
+import sys
+
+dirname = os.path.dirname(__file__)
+if dirname == '':
+    dirname = '.'
+
+dirname = os.path.realpath(dirname)
+updir = os.path.split(dirname)[0]
+if updir not in sys.path:
+    sys.path.append(updir)
+
+###
+
+import plot_figs
+
+class PlotFigsTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_main(self):
+        try:
+            plot_figs.main()
+        except:
+            self.fail()
+
+    def tearDown(self):
+        # Remove files
+        files = ("arm_Kambara_c_forearm.png", "muscle_Kambara_k.png",
+                 "muscle_Kambara_lr.png", "muscle_Li_fa.png",
+                 "muscle_Li_fl.png", "muscle_Li_nf.png",
+                 "muscle_Mitrovic_lm.png", "muscle_Mitrovic_v.png",
+                 "arm_Mitrovic_c_forearm.png", "muscle_Kambara_lm.png",
+                 "muscle_Kambara_v.png", "muscle_Li_fe.png",
+                 "muscle_Li_fv.png", "muscle_Mitrovic_k.png",
+                 "muscle_Mitrovic_lr.png")
+        try:
+            map(os.remove, files)
+        except:
+            pass
+
+###
+
+def test_suite():
+    tests = [unittest.makeSuite(PlotFigsTest)]
+    return unittest.TestSuite(tests)
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
+
