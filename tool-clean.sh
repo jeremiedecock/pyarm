93a963facacbd0ba67ae83b448cb0f4fdf20275f
jeremie.decock.dmz <jeremie.decock.dmz@bb945c2a-ced2-7b97-2cc3-da1a3ce42252>
2011-02-20 20:53:28 +0000

    Update tool-clean script

--- tool-clean.sh
+++ tool-clean.sh
@@ -9,3 +9,11 @@ rm -rf dist
 rm MANIFEST
 rm -rf pyarm_logs
 rm -rf pyarm_figs
+# Debian related files
+rm -rf build-stamp
+rm -rf debian/files
+rm -rf debian/pyarm.postinst.debhelper
+rm -rf debian/pyarm.postrm.debhelper
+rm -rf debian/pyarm.prerm.debhelper
+rm -rf debian/pyarm.substvars
+rm -rf debian/pyarm/
